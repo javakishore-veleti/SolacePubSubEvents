eb8e317d08ac766ba739cc73c942ab62a49ebb57
javakishore-veleti <javakishore@gmail.com>
2021-01-22 14:26:34 -0500

    Initial version of Solace Pub Sub Poc based on a FOREX dummy application

--- forex-trades-main-app/src/main/java/jk/pubsub/solace/forex/trades/main/TradesMainApp.java
+++ forex-trades-main-app/src/main/java/jk/pubsub/solace/forex/trades/main/TradesMainApp.java
@@ -5,8 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TradesMainApp {
 
 	public static void main(String[] args) {
-
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("application-context-all.xml");
-
 	}
 }

ea32107953e1390e36f229fd1e87b77530a62919
javakishore-veleti <javakishore@gmail.com>
2021-01-22 07:43:35 -0500

    Initial version of Solace Pub Sub Poc based on a FOREX dummy application

---
+++ forex-hedging-app/src/main/java/jk/pubsub/solace/forex/hedging/App.java
@@ -0,0 +1,13 @@
+package jk.pubsub.solace.forex.hedging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ forex-interest-rates-app/src/main/java/jk/pubsub/solace/forex/intrates/App.java
@@ -0,0 +1,13 @@
+package jk.pubsub.solace.forex.intrates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
